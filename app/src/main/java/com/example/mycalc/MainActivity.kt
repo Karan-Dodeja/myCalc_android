6cde1a9535fe5024865a583bbcc1fe2cce3a19f4
perceptkraft <141613035+perceptkraft@users.noreply.github.com>
2023-10-02 12:45:41 +0530

    Setup and Architecture Created

---
+++ app/src/main/java/com/example/mycalc/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mycalc
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mycalc.ui.theme.MyCalcTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View){
+        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
+    }
+
+}
